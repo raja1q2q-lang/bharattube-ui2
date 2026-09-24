390c9c4c29d04a345cb17af0c99bfc982b84bcec
virat singh rathore <raja1q2q@gmail.com>
2026-09-24 12:36:26 +0530

    Add files via upload

---
+++ middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/03~yq9q893hmn.js"
+  ],
+  "lowPriorityFiles": [
+    "static/T_DW4I8vqgPmVoRGQz1ac/_buildManifest.js",
+    "static/T_DW4I8vqgPmVoRGQz1ac/_ssgManifest.js",
+    "static/T_DW4I8vqgPmVoRGQz1ac/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/0bzupvr5gt3k9.js",
+    "static/chunks/0pqt~8bl3ukh4.js",
+    "static/chunks/07lhk_q6pmm3r.js",
+    "static/chunks/02g3221oh~3le.js",
+    "static/chunks/turbopack-0l.eg18p.4nbp.js"
+  ]
+};
---
+++ server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PVlTyPA5k9OwO5zmthp/DcCTSG1FBx51DVbHICFxsIg=\"\n}"
